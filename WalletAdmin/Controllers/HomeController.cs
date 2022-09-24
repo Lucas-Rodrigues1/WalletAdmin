ab36d6b866af3ffc0c3c72ef96ba7220f97be68f
Lucas Rodrigues <lucasrodrigues22020@gmail.com>
2022-09-24 11:13:32 -0300

    bora

--- WalletAdmin/Controllers/HomeController.cs
+++ WalletAdmin/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WalletAdmin.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Profile()
         {
             return View();
 
