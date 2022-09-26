9208eb6e07c4bd59f10c2d7554b0c3e9da9a4003
Lucas Eduardo de Souza Rodrigues <lucas.rodrigues@multisoftware.com.br>
2022-09-26 16:54:13 -0300

    boRA

--- WalletAdmin/Controllers/LoginController.cs
+++ WalletAdmin/Controllers/LoginController.cs
@@ -3,17 +3,48 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WalletAdmin.Database;
 using NHibernate;
-using WalletAdmin.Database;
+
 using Microsoft.Extensions.DependencyInjection;
+using WalletAdmin.Repositorio;
+using System.Data.SqlClient;
 
 namespace WalletAdmin.Controllers
 {
     public class LoginController : Controller
     {
-       
+        private readonly UsuariosRepositorio usuariosRepositorio;
+
+        public LoginController(NHibernate.ISession session) => usuariosRepositorio = new UsuariosRepositorio(session);
+
         public ActionResult Index()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string Usuario, string Senha)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-5INB15I\SQLEXPRESS;Initial Catalog=WalletAdmin;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+
+            string login = "SELECT * FROM Tabela_Usuarios WHERE USUARIO='" + Usuario + "' AND SENHA='" + Senha + "'";
+            cmd = new SqlCommand(login, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                con.Close();
+                return RedirectToAction("Index","Home");
+
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
         }
     }
 }
